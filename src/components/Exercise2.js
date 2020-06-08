47e994a2817a8986c0f7805a8a767151aed359c2
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-06-08 15:40:09 +0300

    exs

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 
 class Exercise2 extends Component {
 
-constructor() {
-    super()
-    this.state = {
-        name: "",
-        fruit: ""
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            fruit: ""
+        }
     }
-}
 
-render() {
-    return (
-        <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
-        </div>
-    );
-}
+    updateName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    updateFruit = async (event) => {
+        await this.setState({
+            fruit: event.target.value
+        })
+        console.log(this.state.name + ' loves ' + this.state.fruit)
+    }
+
+    render() {
+        return (
+            <div>
+                <input id="name-input" onChange={this.updateName} />
+                <select id="select-input" onChange={this.updateFruit}>
+                    <option value={'apple'}>Tapuach</option>
+                    <option value={'orange'}>Tapuz</option>
+                    <option value={'pineapple'}>Ananas</option>
+                </select>
+            </div>
+        )
+    }
 }
 
 export default Exercise2;
